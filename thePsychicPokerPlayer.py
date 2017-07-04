b82d8c30fd092b8e5f519f8af3c149a19865530a
orizhiy <sklif.85@gmail.com>
2017-07-04 20:37:35 +0300

    v.4_brute-force_search

--- thePsychicPokerPlayer.py
+++ thePsychicPokerPlayer.py
@@ -3,8 +3,13 @@
 from __future__ import print_function
 import sys
 
-stdin = ['A B C 1 2 3']
-# stdin = ['TH JH QC QD QS QH KH AH 2S 6S']
+class CardsCombination:
+
+
+
+
+# stdin = ['A B C 1 2 3']
+stdin = ['TH JH QC QD QS QH KH AH 2S 6S']
 #          '2H 2S 3H 3S 3C 2D 3D 6C 9C TH',
 #          '2H 2S 3H 3S 3C 2D 9C 3D 6C TH',
 #          '2H AD 5H AC 7H AH 6H 9H 4H 3C',
@@ -14,8 +19,9 @@ stdin = ['A B C 1 2 3']
 #          '6C 9C 8C 2D 7C 2H TC 4C 9S AH',
 #          '3D 5S 2H QD TD 6S KH 9H AD QH']
 
-# solve = list()
 combination = None
+fp = open('./test.txt', 'a')
+
 
 def solve_print(combination, hand, deck):
     print("Hand: ", end='')
@@ -26,27 +32,26 @@ def solve_print(combination, hand, deck):
     print(combination)
     exit(0)
 
-def check_straight_flush(solve, list):
-    return 0
-
 def check(solve):
     return 0
 
-def solution(hand, deck, solve=None, h=None):
-    if solve is None:
-        solve = [n for n in deck]
-        h = [n for n in hand]
-        solution(hand, deck, solve, h)
+def solution(hand, deck, _hand=None, _deck=None):
+    if _hand is None:
+        _hand = [n for n in hand]
+        _deck = [n for n in deck]
+        solution(hand, deck, _hand, _deck)
     else:
-        if len(solve) == 0:
+        if (len(_deck) == 0):
             return
-        for i in range(0, len(hand)):
-            s = [n for n in h]
-            for j in range(0, len(s)):
-                if s[j] not in deck:
-                    s[i] = solve[0]
-            print(s)
-            solution(hand, deck, solve[1:], s)
+        else:
+            for i in range(len(hand)):
+                solve = [n for n in _hand]
+                if solve[i] not in deck:
+                    solve[i] = _deck[0]
+                    # print(solve, file=fp)
+                    # print(solve)
+                    check(solve)
+                solution(hand, deck, solve, _deck[1:])
     return
 
 # for line in sys.stdin:
@@ -54,17 +59,4 @@ for line in stdin:
     line = line.split(' ')
     hand = line[0:len(line)/2]
     deck = line[len(line)/2:len(line)]
-    # solve = init_solve_list(solve, len(deck))
-    # if check_straight_flush(solve, hand):
-    #     solve_print(combination, hand, deck)
-    # if check_straight_flush(solve, deck):
-    #     solve_print(combination, hand, deck)
-
-    # solution(init_solve_list(solve, len(deck)), hand, deck)
-    # solve = lambda deck: [None] * len(deck)
     solution(hand, deck)
-
-    # n = 1
-    # solution(solve, hand, deck[0:len(deck) - n], n, 0, 0, 0)
-    # solution(init_solve_list(solve, len(deck)), hand, deck, 0, 0)
-    # print(res)
