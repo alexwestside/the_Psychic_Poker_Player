f881b95ee6a82e7e9ebff84fe4925cae1c47c7ed
orizhiy <sklif.85@gmail.com>
2017-07-05 22:07:02 +0300

    class CardsCombination_v.6

--- thePsychicPokerPlayer.py
+++ thePsychicPokerPlayer.py
@@ -4,18 +4,6 @@ from __future__ import print_function
 import sys
 from collections import Counter
 
-stdin = [('TH JH QC QD QS QH KH AH 2S 6S'),
-         ('2H 2S 3H 3S 3C 2D 3D 6C 9C TH'),
-         ('2H 2S 3H 3S 3C 2D 9C 3D 6C TH'),
-         ('2H AD 5H AC 7H AH 6H 9H 4H 3C'),
-         ('AC 2D 9C 3S KD 5S 4D KS AS 4C'),
-         ('KS AH 2H 3C 4H KC 2C TC 2D AS'),
-         ('AH 2C 9S AD 3C QH KS JS JD KD'),
-         ('6C 9C 8C 2D 7C 2H TC 4C 9S AH'),
-         ('3D 5S 2H QD TD 6S KH 9H AD QH')]
-
-fp = open('./test.txt', 'a')
-
 class CardsCombination:
     combination = {None: 0}
     combinations = {1: {'straight-flush': 1},
@@ -179,12 +167,12 @@ def solution(hand, deck, _hand=None, _deck=None):
                     solution(hand, deck, solve, _deck[1:])
     return
 
-# for line in sys.stdin:
-for line in stdin:
+for line in sys.stdin:
     res = CardsCombination()
     res.combination = {None: 0}
     line = line.split(' ')
     hand = line[0:len(line) / 2]
     deck = line[len(line) / 2:len(line)]
+    res.combinations(hand)
     solution(hand, deck)
     res.solve_print(res.combination.keys()[0], hand, deck)
